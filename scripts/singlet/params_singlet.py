94a476617c71f6ec00f961697520edf59e58b9d0
Lauri Niemi <lauri.b.niemi@helsinki.fi>
2022-08-04 14:38:14 +0300

    Improved singlet parameter conversion script, much cleaner than before

--- scripts/singlet/params_singlet.py
+++ scripts/singlet/params_singlet.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ## This script reads in 3d continuum parameters from 'input file' at a given temperature,
 ## then converts and prints out the corresponding lattice parameters.
@@ -9,17 +9,17 @@
 
 import sys
 import numpy as np
-from scipy.optimize import fsolve
 import math
 from pylab import genfromtxt
 
 
+## Find index of nearest value in array
 def nearest_indexOf(array, value):
     array = np.asarray(array)
     index = (np.abs(array - value)).argmin()
     return index
 
-
+## Find nearest value in array
 def nearest(array, value):
     array = np.asarray(array)
     index = (np.abs(array - value)).argmin()
@@ -30,97 +30,88 @@ def lin_int(x, b, k):
 	return b + k*x
 
 
-## read in temperature T and beta_G
-if not len(sys.argv) in [5,6]:
-    sys.stderr.write('Usage: %s <input file> <T> <beta_G> <r_u1> <volume (optional)>\n' %
-                     sys.argv[0])
-    sys.exit(1)
-
-datafile = sys.argv[1]
-T = float(sys.argv[2])
-betaIn = float(sys.argv[3])
-r_u1 = float(sys.argv[4])
-
-if len(sys.argv) == 6:
-    volume = int(sys.argv[5])
-
-if not (r_u1.is_integer()):
-    print("!!! r_u1 = %.16f is not an integer, does not define U(1) irrep\n" % r_u1)
-
-params = genfromtxt(datafile, names=True)
-
-temps = params['T']
+## Find parameter labeled 'name' in the input array at given T. Will interpolate if needed
+def get_param(paramList, name, T):
+    temperatures = paramList['T']
+    p = paramList[name]
+    res = 0
+    if T in temperatures:
+        res = p[np.where(temperatures == T)][0]
+    else: 
+        ## linear interpolation around T
+        i = nearest_indexOf(temperatures, T)
+        T1 = temperatures[i-1]
+        T2 = temperatures[i+1]
+        p1 = p[np.where(temperatures == T1)][0]
+        p2 = p[np.where(temperatures == T2)][0]
+        k = (p2 - p1) / (T2 - T1)
+        b = p1 - k * T1
+        res = b + k * T
+    return res
 
 
-## find parameter labelled 'name' in the input file at given T
-def get_param(name, T):
-    if T not in temps:
-        print("Error in get_param()!!\n")
-        sys.exit(2)
-    line = params[np.where(temps == T)]
-    par = line[name][0]
-    return par
+## get all parameters from the input file. Returns a dictionary
+def getAllParams(paramList, T):
+    
+    res = {}
+    res["T"] = T
+    res["gsq"] = get_param(paramList, 'gsq', T)
+    res["gpsq"] = get_param(paramList, 'gpsq', T)
+    res["mphisq"] = get_param(paramList, 'mphisq', T)
+    res["lambda"] = get_param(paramList, 'lam', T)
+    res["mSsq"] = get_param(paramList, 'mSsq', T)
+    res["b1"] = get_param(paramList, 'b1', T)
+    res["b3"] = get_param(paramList, 'b3', T)
+    res["b4"] = get_param(paramList, 'b4', T)
+    res["a1"] = get_param(paramList, 'a1', T)
+    res["a2"] = get_param(paramList, 'a2', T)
+    res["RGscale"] = get_param(paramList, 'RGscale', T)
 
-## get all parameters from the input file
-def getAllParams(T):
+    return res
 
-    gsq = get_param('gsq', T)
-    gpsq = get_param('gpsq', T)
-    mphisq = get_param('mphisq', T)
-    lam = get_param('lam', T)
-    mSsq = get_param('mSsq', T)
-    b1 = get_param('b1', T)
-    b3 = get_param('b3', T)
-    b4 = get_param('b4', T)
-    a1 = get_param('a1', T)
-    a2 = get_param('a2', T)
-    RGscale = get_param('RGscale', T)
+## Calculate dp/dT ~ (p2 - p1) / (T2 - T1) for each parameter p in the input dictionaries. 
+## Assumes that both dictionaries contain element named 'T', the temperature
+def CalcTDerivatives(paramDict1, paramDict2):
+    dT = 0
+    T1 = paramDict1["T"]
+    T2 = paramDict2["T"]
+    dT = T2 - T1
+    if (dT == 0): 
+        print("!!! Error in CalcTDerivatives(), invalid input given")
+        exit(13)
 
-    ## return as a list. Use this ordering from now on!!
-    return [gsq, gpsq, mphisq, lam, mSsq, b1, b3, b4, a1, a2, RGscale]
+    res = {}
+    for key, value in paramDict1.items():
+        p1 = value
+        p2 = paramDict2[key] 
+        res[key] = (p2 - p1) / dT
 
-
-## write params and T to a file
-def write_params(fname, plist, T, islat):
-
-	## if islat = 1, last parameter in plist is the scaling a, otherwise it is the MSbar scale
-    if (islat):
-        scale_id = 'spacing'
-    else:
-        scale_id = 'RGscale'
-
-    [gsq, gpsq, mphisq, lam, mSsq, b1, b3, b4, a1, a2, scale] = plist
-
-    f = open(fname, "w+")
-
-    f.write('T %.16f\n' % T)
-    f.write('%s %.16f\n' % (scale_id, scale))
-
-		## for lattice params, the gsq and gpsq are actually beta and betaU1
-    if (islat):
-        f.write('beta %.16f\n' % beta)
-        f.write('betaU1 %.16f\n' % gpsq)
-        f.write('r_U1 %.16f\n' % r_u1)
-    else:
-    	f.write('gsq %.16f\n' % gsq)
-    	f.write('gpsq %.16f\n' % gpsq)
-
-    f.write('mphisq %.16f\n' % mphisq)
-    f.write('lambda %.16f\n' % lam)
-    f.write('mSsq %.16f\n' % mSsq)
-    f.write('b1 %.16f\n' % b1)
-    f.write('b3 %.16f\n' % b3)
-    f.write('b4 %.16f\n' % b4)
-    f.write('a1 %.16f\n' % a1)
-    f.write('a2 %.16f\n' % a2)
-
-    f.close()
+    return res
 
 
-#### convert to lattice parameters
-def convert_lattice(p_cont, spacing):
 
-    [gsq, gpsq, muphisq, lam, muSsq, b1, b3, b4, a1, a2, RGscale] = p_cont
+## Write a parameter dictionary to a file
+def write_params(fname, pdict):
+
+    with open(fname, 'w') as f: 
+        for key, value in pdict.items(): 
+            f.write('%s %s\n' % (key, value))
+
+
+#### Convert to lattice parameters. p_cont and return value are dictionaries
+def convert_lattice(p_cont, spacing, r_u1):
+
+    gsq = p_cont["gsq"]
+    gpsq = p_cont["gpsq"]
+    muphisq = p_cont["mphisq"]
+    lam = p_cont["lambda"]
+    b1 = p_cont["b1"]
+    muSsq = p_cont["mSsq"]
+    b3 = p_cont["b3"]
+    b4 = p_cont["b4"]
+    a1 = p_cont["a1"]
+    a2 = p_cont["a2"]
+    RGscale = p_cont["RGscale"]
 
     # generic constants
     Sigma = 3.17591153625
@@ -141,7 +132,7 @@ def convert_lattice(p_cont, spacing):
 
 
     ## Higgs mass counterterms. First contributions from SU(2) + Higgs only
-    mphisq_ct1 = -Sigma/(8.0*math.pi*a) * (3*gsq + gpsq + 12*lam);
+    mphisq_ct1 = -Sigma/(8.0*math.pi*a) * (3*gsq + gpsq + 12*lam)
 
     mphisq_ct2 = 1.0/(16*math.pi**2.0) * ( \
         ( -51.0/16.0*gsq**2 + 9.0/8.0*gsq*gpsq \
@@ -155,27 +146,27 @@ def convert_lattice(p_cont, spacing):
         )
 
     ## then contributions from the singlet
-    mphisq_ct1 += -a2*Sigma/(8.0*a*math.pi);
+    mphisq_ct1 += -a2*Sigma/(8.0*a*math.pi)
     mphisq_ct2 += 1.0/(16*math.pi**2.0) * 0.5*a2**2 * logPlusZeta;
 
     mphisq_lat = (muphisq + mphisq_ct1 + mphisq_ct2) * a**2
 
 
     ## singlet tadpole term
-    b1_ct1 = -Sigma/(4.0*a*math.pi) * (b3 + a1);
+    b1_ct1 = -Sigma/(4.0*a*math.pi) * (b3 + a1)
     b1_ct2 = 1.0/(16.0*math.pi**2) * (  \
         (2.0*b3*b4 + a1*a2 - 0.5*a1* (3*gsq + gpsq)) * logPlusZeta \
         + 0.5*a1 * (3*gsq + gpsq)*(delta - Sigma**2 / 4.0) \
-    );
+    )
 
     b1_lat = (b1 + b1_ct1 + b1_ct2) * a**(5.0/2.0)
 
     ## singlet mass counterterms
-    mSsq_ct1 = -Sigma/(4.0*a*math.pi) * (2*a2 + 3*b4);
+    mSsq_ct1 = -Sigma/(4.0*a*math.pi) * (2*a2 + 3*b4)
     mSsq_ct2 = 1.0/(16.0*math.pi**2) * ( \
         (2*a2**2 + 6*b4**2 - a2* (3*gsq + gpsq)) * logPlusZeta \
         + a2 * (3*gsq + gpsq)*(delta - Sigma**2 / 4.0) \
-    );
+    )
 
     mSsq_lat = (muSsq + mSsq_ct1 + mSsq_ct2) * a**2.0
 
@@ -193,96 +184,117 @@ def convert_lattice(p_cont, spacing):
     a1_lat = a1 * a**(3.0/2.0)
     a2_lat = a2 * a
 
-    return [beta, betau1, mphisq_lat, lam_lat, mSsq_lat, b1_lat, b3_lat, b4_lat, a1_lat, a2_lat, a]
+    res = {
+        "spacing" : a,
+        "beta" : beta,
+        "betaU1" : betau1,
+        "r_U1" : r_u1,
+        "mphisq" : mphisq_lat,
+        "lambda" : lam_lat,
+        "mSsq" : mSsq_lat,
+        "b1" : b1_lat,
+        "b3" : b3_lat,
+        "b4" : b4_lat,
+        "a1" : a1_lat,
+        "a2" : a2_lat,
+        "T" : p_cont["T"]
+    }
+
+    return res
 
 
-####### end function definitions #########
+####### Begin main #########
+
+def main(): 
+    ## read in temperature T and beta_G
+    if not len(sys.argv) in [5,6]:
+        sys.stderr.write('Usage: %s <input file> <T> <beta_G> <r_u1> <volume (optional)>\n' %
+                        sys.argv[0])
+        sys.exit(1)
+
+    datafile = sys.argv[1]
+    T = float(sys.argv[2])
+    betaIn = float(sys.argv[3])
+    r_u1 = float(sys.argv[4])
+
+    if len(sys.argv) == 6:
+        volume = int(sys.argv[5])
+
+    if not (r_u1.is_integer()):
+        print("!!! r_u1 = %.16f is not an integer, does not define U(1) irrep\n" % r_u1)
+
+    paramList = genfromtxt(datafile, names=True)
+
+    ## Will interpolate if the requested T was not found in file
+    temperatures = paramList['T']
+    bInterpolate = (T not in temperatures)
+
+    if (bInterpolate):
+        print('\n   ! No direct match for the temperature found; interpolating...\n')
+
+    index = nearest_indexOf(temperatures, T)
+    if (index-1 < 0 or index+1 >= len(temperatures)):
+        print("!!! Found too few T-values around T=%g, cannot calculate T-derivatives. Use a file with broader T-range." % T)
+        exit(11)
+
+    ## Read the continuum parameters
+    params_cont = getAllParams(paramList, T)
+
+    print('\n---- Read in continuum 3d parameters at T = '+str(T)+' ----')
+    print(params_cont)
+    ## write these into a file
+    write_params('params_continuum.dat', params_cont)
 
 
-##### Find input parameters at the given T, interpolate if needed
-inputs = []
-if T not in temps:
-    print('\nNo direct match for the temperature found; interpolating...\n')
-    index = nearest_indexOf(temps, T)
-    T1 = temps[index-1]
-    T2 = temps[index+1]
-    p1 = getAllParams(T1)
-    p2 = getAllParams(T2)
-    for i in range(0, len(p1)):
-        k = 1.0*(p2[i]-p1[i])/(T2 - T1)
-        b = p1[i] - k * T1
-        inputs.append( lin_int(T, b, k) ) ## uses same order as getAllParams()
-else:
-    inputs = getAllParams(T)
+    ## Calculate lattice spacing here, keep fixed when computing T-derivatives
+    gsq_cont = params_cont["gsq"]
 
 
-[gsq, gpsq, mphisq, lam, mSsq, b1, b3, b4, a1, a2, RGscale] = inputs
+    spacing = 4.0 / (betaIn * gsq_cont)
 
-print
-print('---- Read in continuum 3d parameters at T = '+str(T)+' ----')
-
-print('gsq %g, gpsq %g, mphisq %g, lambda %g, mSsq %g, b1 %g, b3 %g, b4 %g, a1 %g, a2 %g, RGscale %g\n\n'
-                     % (gsq, gpsq, mphisq, lam, mSsq, b1, b3, b4, a1, a2, RGscale))
+    
+    ## lattice parameters
+    params_lat = convert_lattice(params_cont, spacing, r_u1)
 
 
-## write these into a file
-write_params('params_continuum.dat', inputs, T, 0)
+    print('\n---- Lattice parameters for beta_G = '+str(betaIn)+' ----')
+    print(params_lat)
+    write_params('params_lattice.dat', params_lat)
 
 
-gsq_cont = gsq
-a2_cont = a2
+    print('\nScales (GeV): g^2 T %g, gT %g, a2 T %g, lattice cutoff %g' % (gsq_cont, math.sqrt(gsq_cont) * math.sqrt(T), params_cont["a2"], math.pi/spacing))
 
-## calculate lattice spacing from g_3^2 and beta:
-spacing = 4.0 / (betaIn * gsq)
+    ### Calculate reweight string if the volume was given ###
+    if len(sys.argv) == 6:
+        sidelen = volume**(1.0/3.0)
+        print('Length scales (1/GeV): 1/(g^2 T) %g, lattice side %g' % (1.0/gsq_cont, spacing*sidelen))
+        print('\n')
 
-## lattice parameters
-paramsLat = convert_lattice(inputs, spacing)
-[beta, betau1, mphisq, lam, mSsq, b1, b3, b4, a1, a2, a] = paramsLat
+        ## First get continuum parameters at two nearby temperatures
+        i = nearest_indexOf(temperatures, T)
+        T1 = temperatures[i-1]
+        T2 = temperatures[i+1]
 
-print('---- Lattice parameters for beta_G = '+str(betaIn)+' ----')
+        p1_cont = getAllParams(paramList, T1)
+        p2_cont = getAllParams(paramList, T2)
 
-print('beta %g, betau1 %g, mphisq %g, lambda %g, mSsq %g, b1 %g, b3 %g, b4 %g, a1 %g, a2 %g, a %g\n\n'
-                     % (beta, betau1, mphisq, lam, mSsq, b1, b3, b4, a1, a2, a))
+        ## And convert them to lattice params
+        p1_lat = convert_lattice(p1_cont, spacing, r_u1)
+        p2_lat = convert_lattice(p2_cont, spacing, r_u1)
 
-
-write_params('params_lattice.dat', paramsLat, T, 1)
-
-
-print('Scales (GeV): g^2 T %g, gT %g, a2 T %g, lattice cutoff %g' % (gsq_cont, math.sqrt(gsq_cont) * math.sqrt(T), a2_cont, 1.0/spacing))
+        dpdT = CalcTDerivatives(p1_lat, p2_lat)
 
 
-### Calculate reweight string if the volume was given ###
-if len(sys.argv) == 6:
-    sidelen = volume**(1.0/3.0)
-    print('Length scales (1/GeV): 1/(g^2 T) %g, lattice side %g' % (1.0/gsq_cont, spacing*sidelen))
-    print('')
+        ## reweight string is dS/dT, but we measure volume averages instead of sums of type sum_x O(x) => multiple by volume here
+        print('---- Reweight string ----\n')
+        print( """%g * ( %.16g*#4 + %.16g*#5 + %.16g*#7 + %.16g*#8 +%.16g*#9 + %.16g*#10 + %.16g*#11 + %.16g*#12 + %.16g*#13 + %.16g*#14)""" % 
+            (volume, dpdT["beta"], dpdT["betaU1"], dpdT["mphisq"], dpdT["lambda"], dpdT["b1"], dpdT["mSsq"], 
+                dpdT["b3"], dpdT["b4"], dpdT["a1"], dpdT["a2"]))
 
-    ## linearize in T
-    T1 = nearest(temps, T-0.1)
-    T2 = nearest(temps, T+0.1)
+        #  #n is the nth column in measure file.
 
-    if (T1 == T or T2 == T):
-    	print('!!! Unable to calculate reweight string; include more values of T in the input file\n')
-    	sys.exit(2)
+    ## end if len(sys.argv) == 6 ###
 
-    p1 = getAllParams(T1)
-    p2 = getAllParams(T2)
-    ## lattice params at the two temperatures; keep 'a' fixed but beta can change to compensate change in g_3^2(T)
-    p1lat = convert_lattice(p1, spacing)
-    p2lat = convert_lattice(p2, spacing)
 
-    k = []
-    for i in range(0, len(p1lat)):
-    	k.append( 1.0*volume * (p2lat[i]-p1lat[i])/(T2-T1) )
 
-    ## k now contains V * d/dT for each parameter in the action (V = number of sites):
-    [beta, betau1, mphisq, lam, mSsq, b1, b2, b3, a1, a2, a] = k ## same ordering as in output of convert_lattice()
-
-    print('---- Reweight string ----\n')
-    print(('%.16f * #4 + %.16f * #5 + %.16f * #7 + %.16f * #8 + %.16f * #9 + %.16f * #10'
-        ' + %.16f * #11 + %.16f * #12 + %.16f * #13 + %.16f * #14') %
-        (beta, betau1, mphisq, lam, b1, mSsq, b3, b4, a1, a2))
-
-    #  #n is the nth column in measure file.
-
-## end if len(sys.argv) == 6 ###
+main()
